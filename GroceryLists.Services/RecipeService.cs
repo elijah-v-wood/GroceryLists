0c31e3dffe1525d3b5e9cae1ff250482c9bef97d
Elijah Wood <elijah.v.wood@gmail.com>
2021-09-09 13:23:01 -0400

    Added RecipeService

---
+++ GroceryLists.Services/RecipeService.cs
@@ -0,0 +1,37 @@
+﻿using GroceryLists.Data;
+using GroceryLists.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryLists.Services
+{
+    public class RecipeService
+    {
+        private readonly Guid _userId;
+
+        public RecipeService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateRecipe(RecipeCreate model)
+        {
+            var entity = new Recipe()
+            {
+                OwnerId = _userId,
+                Name=model.Name,
+                Ingredients=model.Ingredients,
+                Instructions=model.Instructions,
+                Access=model.Access,
+                CreatedUtc=DateTimeOffset.Now
+            };
+            using(var ctx = new ApplicationDbContext())
+            {
+                ctx.Recipes.Add(entity);
+            }
+        }
+    }
+}
