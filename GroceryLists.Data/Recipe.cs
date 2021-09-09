3029e93d36184082acf5b1ae4189b3814733004e
Elijah Wood <elijah.v.wood@gmail.com>
2021-09-09 11:03:01 -0400

    Added Recipe Model to data

---
+++ GroceryLists.Data/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryLists.Data
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string Name { get; set; }
+        public Ingredient[] Ingredients { get; set; }
+        private bool Access { get; set; } = false;
+
+        public void PublicAccess()
+        {
+            Access = true;
+        }
+
+
+    }
+}
